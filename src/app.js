8fe213a9efbbbee41fd3de4789e20439c4c955c5
Kou Guandong <gdkou90@gmail.com>
2017-10-09 20:05:59 +0800

    feat: finished contacts module layout and styles

--- src/app.js
+++ src/app.js
@@ -1,16 +1,6 @@
 $(() => {
   
-  async function hello() {
-    console.log('into app.js');
-    await setTimeout(() => {
-      const el = document.createElement('h1');
-      el.innerHTML = 'hello, world~';
-      document.body.appendChild(el);
-    }, 1000);
-    
-  }
-  
-  hello();
+  console.log('into js');
   
 });
 
