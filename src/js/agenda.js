dd5539ff8726ac818be68fd98420959219abf51e
Kou Guandong <gdkou90@gmail.com>
2017-10-14 14:19:17 +0800

    feat: finished toggling agenda detail-brief view mode

--- src/js/agenda.js
+++ src/js/agenda.js
@@ -1,9 +1,11 @@
 $(() => {
   
   console.log('into agenda');
-  const $avatar = $('#agenda .avatar');
+  const $avatar = $('#agenda .avatar-wrapper');
   const $controlUnit = $('.control-unit');
   const $info = $('#agenda .info-wrapper');
+  const $people = $('#agenda .people-wrapper');
+  
   
   $controlUnit.on(click, function(e) {
     $controlUnit.removeClass('active');
@@ -11,9 +13,11 @@ $(() => {
     if (!this.hasAttribute('data-detail')) {
       $avatar.addClass('hidden');
       $info.addClass('static');
+      $people.addClass('brief');
     } else {
       $avatar.removeClass('hidden');
       $info.removeClass('static');
+      $people.removeClass('brief');
     }
   })
 
