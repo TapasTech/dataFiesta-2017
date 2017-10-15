1e66d37b4b3a4d0d8348b9fb04a909b344230f1a
Kou Guandong <gdkou90@gmail.com>
2017-10-15 17:40:37 +0800

    feat: added UI details for tickets module

---
+++ src/js/ticket.js
@@ -0,0 +1,16 @@
+$(function () {
+  
+  console.log('ticket');
+  
+  var tickets = [
+    {
+      img: 'normal',
+      link: 'fake-url-normal-ticket'
+    },
+    {
+      img: 'VIP',
+      link: 'fake-url-VIP-ticket'
+    }
+  ]
+  
+});
