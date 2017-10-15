f88ed594d639e8be54691477894c0eada6765e39
Kou Guandong <gdkou90@gmail.com>
2017-10-15 17:02:37 +0800

    feat: added animation effect for navbar clicks

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,5 +1,15 @@
 $(() => {
   
   console.log('navbar');
+  const $navItem = $('a.nav-item');
+  
+  $navItem.on(CLICK, function(e) {
+    const $this = $(this);
+    if (!$this.attr('target')) {
+      e.preventDefault();
+      const $href = $($this.attr('href'));
+      $('html').animate({scrollTop: $href.offset().top - 80}, 600);
+    }
+  })
   
 });
