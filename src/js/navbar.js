f575a7c14fd27b71adb5536a89911436f368cbcc
Kou Guandong <gdkou90@gmail.com>
2017-10-16 21:32:09 +0800

    feat: UI compatibility for mobiles in dev

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -2,6 +2,8 @@ $(() => {
   
   console.log('navbar');
   const $navItem = $('a.nav-item');
+  const $menusWrapper = $('.menus-wrapper');
+  const $menuMobile = $('.menu-btn');
   
   $navItem.on(CLICK, function(e) {
     const $this = $(this);
@@ -9,7 +11,15 @@ $(() => {
       e.preventDefault();
       const $href = $($this.attr('href'));
       $('html').animate({scrollTop: $href.offset().top - 80}, 600);
+      
+      $menusWrapper.removeClass('expanded');
+      
     }
+  });
+  
+
+  $menuMobile.on(CLICK, function() {
+    $menusWrapper.toggleClass('expanded');
   })
   
 });
