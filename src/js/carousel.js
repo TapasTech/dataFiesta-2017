b47091fc9e75712e356435c1c251032a9082478a
Kou Guandong <gdkou90@gmail.com>
2017-10-17 16:07:18 +0800

    chore: fixing UI issue for carousel mobile

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -5,9 +5,11 @@ $(() => {
   const mouseEnter = 'mouseenter';
   const $carouselPerson = $('#carousel .person-unit');
   
+  const gap = IS_MOBILE? 190 : 420;
+  
   $carouselPerson.on(mouseEnter, function(e) {
     var $detail = $(this).find('.detail-wrapper');
-    if (WIDTH - e.clientX < 420) {
+    if (WIDTH - e.clientX < gap) {
       $detail.addClass('move-left');
     } else {
       $detail.removeClass('move-left');
