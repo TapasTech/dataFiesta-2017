0c95f8440edc7c5bab191461396ba6cf7a407c1e
Kou Guandong <gdkou90@gmail.com>
2017-10-18 15:15:31 +0800

    feat: locate pop-up modal of detail at the center of carousel module

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -3,17 +3,41 @@ $(() => {
   console.log('carousel');
   
   const mouseEnter = 'mouseenter';
-  const $carouselPerson = $('#carousel .person-unit');
+  const personUnit = $('#carousel .person-unit');
   
-  const gap = IS_MOBILE? 190 : 420;
+  personUnit.on(mouseEnter, function(e) {
+    const $this = $(this);
+    if (IS_MOBILE) {
+      e.stopPropagation();
+      const attributes = ['name', 'job', 'type', 'title', 'time'];
+      const values = [];
+      attributes.forEach(item => {
+        values.push($this.attr('data-' + item));
+      });
+      const [name, job, type, title, time] = values;
+      const $popup = $('.detail-popup');
+      $popup.removeClass('hidden');
+      const $text = $popup.find('.text-wrapper');
+      const $avatar = $popup.find('.avatar');
+      $text.find('.job').html(job);
+      $text.find('.type').html(type);
+      $text.find('.title').html(title);
+      $text.find('.time').html(time);
+      $avatar.css("backgroundImage", `url(http://z.dtcj.com/cbndata/avatars/${name}.png)`);
+      return;
+    }
   
-  $carouselPerson.on(mouseEnter, function(e) {
-    var $detail = $(this).find('.detail-wrapper');
+    const gap = IS_MOBILE? 190 : 420;
+    var $detail = $this.find('.detail-wrapper');
     if (WIDTH - e.clientX < gap) {
       $detail.addClass('move-left');
     } else {
       $detail.removeClass('move-left');
     }
+  });
+  
+  $('body').on(CLICK, function(e) {
+    $('.detail-popup').addClass('hidden');
   })
   
 });
